3dd91d8cce8f2a17c81ffe9b67a553fc60733c8b
Yousef Mostafa <52266199+yousefmostafa200@users.noreply.github.com>
2024-06-13 18:01:12 +0300

    Time: 145 ms (27.69%), Space: 58.3 MB (35.39%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    if(x < 0) return false;
+
+    const str = x.toString();
+
+   const reversed = str.split('').reverse().join('')
+
+   return str === reversed;
+};
