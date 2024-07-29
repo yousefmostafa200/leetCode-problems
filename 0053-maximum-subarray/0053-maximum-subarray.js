fe26c1d098abf2cb2bfc5b43c8529c6d23dbadbc
Yousef Mostafa <52266199+yousefmostafa200@users.noreply.github.com>
2024-07-29 09:28:22 +0300

    Time: 84 ms (12.53%), Space: 57.5 MB (63.37%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -2,14 +2,14 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maxSubArray = function(arr) {
+var maxSubArray = function (arr) {
     let maxEndingHere = arr[0];
-  let maxSoFar = arr[0];
+    let maxSoFar = arr[0];
 
-  for (let i = 1; i < arr.length; i++) {
-    maxEndingHere = Math.max(arr[i], maxEndingHere + arr[i]);
-    maxSoFar = Math.max(maxEndingHere, maxSoFar);
-  }
+    for (let i = 1; i < arr.length; i++) {
+        maxEndingHere = Math.max(arr[i], maxEndingHere + arr[i]);
+        maxSoFar = Math.max(maxEndingHere, maxSoFar);
+    }
 
-  return maxSoFar;
+    return maxSoFar;
 };
