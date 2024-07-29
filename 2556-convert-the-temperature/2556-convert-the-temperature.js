3d5de64876145636d646f4e6ef6b72c369a966d8
Yousef Mostafa <52266199+yousefmostafa200@users.noreply.github.com>
2024-07-29 21:27:37 +0300

    Time: 45 ms (85.33%), Space: 48.8 MB (46.88%) - LeetHub

--- 2556-convert-the-temperature/2556-convert-the-temperature.js
+++ 2556-convert-the-temperature/2556-convert-the-temperature.js
@@ -4,5 +4,9 @@
  */
 var convertTemperature = function(celsius) {
    
-    return [celsius + 273.15,  celsius * 1.80 + 32.00]
+    // return [celsius + 273.15,  celsius * 1.80 + 32.00]
+
+    let kelvin  = (celsius +273.15).toFixed(5);
+    let farenheit=((celsius * 1.8) +32).toFixed(5);
+    return ([kelvin,farenheit])
 };
