78a8da1f1f0ebce2f0b09b822b227052ea4a1537
Yousef Mostafa <52266199+yousefmostafa200@users.noreply.github.com>
2024-08-18 23:08:03 +0300

    Time: 59 ms (21.42%), Space: 48.8 MB (59.82%) - LeetHub

--- 1797-goal-parser-interpretation/1797-goal-parser-interpretation.js
+++ 1797-goal-parser-interpretation/1797-goal-parser-interpretation.js
@@ -3,18 +3,19 @@
  * @return {string}
  */
 var interpret = function(command) {
-    let result = '';
-    for(let i =0; i< command.length; i++){
-        if(command[i] === 'G'){
-            result += 'G'
-        } else if(command[i] === '(' && command[i+1] === ')'){
-            result += 'o';
-            i++;
-        } else if (command[i] === "(" && command[i + 1] === "a" &&
-         command[i + 2] === "l" && command[i + 3] === ")"){
-            result += 'al';
-            i+=3
-         }
-    }
-    return result
+    // let result = '';
+    // for(let i =0; i< command.length; i++){
+    //     if(command[i] === 'G'){
+    //         result += 'G'
+    //     } else if(command[i] === '(' && command[i+1] === ')'){
+    //         result += 'o';
+    //         i++;
+    //     } else if (command[i] === "(" && command[i + 1] === "a" &&
+    //      command[i + 2] === "l" && command[i + 3] === ")"){
+    //         result += 'al';
+    //         i+=3
+    //      }
+    // }
+    // return result
+    return command.split('()').join('o').split('(al)').join('al')
 };
