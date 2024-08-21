bf0a7561b4340919063d937546e7af2a685f0402
Yousef Mostafa <52266199+yousefmostafa200@users.noreply.github.com>
2024-08-21 22:25:48 +0300

    Time: 71 ms (92.5%), Space: 52.7 MB (87.47%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -5,16 +5,16 @@
 var lengthOfLongestSubstring = function(s) {
     if(s.length <= 1) return s.length;
 
-    const seenChars = {}
+    const seenChars = new Map()
     let left =0 ,  longest =0;
 
     for(let right = 0; right < s.length; right++){
         const currChar = s[right];
-        const prevSeenChars = seenChars[currChar];
+        const prevSeenChars = seenChars.get(currChar);
         if(prevSeenChars >= left){
           left =  prevSeenChars  + 1
         }
-        seenChars[currChar] = right;
+        seenChars.set(currChar, right);
         longest = Math.max(longest, right - left + 1)
     }
     return longest
