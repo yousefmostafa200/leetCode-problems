a1e4b8508b6991318cd5a5afbd2d7fdb1851c3a6
Yousef Mostafa <52266199+yousefmostafa200@users.noreply.github.com>
2024-08-28 22:41:13 +0300

    Time: 133 ms (92.68%), Space: 68.6 MB (84.15%) - LeetHub

--- 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.js
+++ 1256-rank-transform-of-an-array/1256-rank-transform-of-an-array.js
@@ -3,21 +3,18 @@
  * @return {number[]}
  */
 var arrayRankTransform = function(arr) {
-    
-
-    const sorted = arr.slice().sort((a,b) => a - b); // [10,20,30,40]
-    const obj = {};
+     const sorted = arr.slice().sort((a,b) => a - b); // [10,20,30,40]
+    const map = new Map();
     let rank = 1;
     for(let i =0; i< sorted.length; i++){
-        if(!obj[sorted[i]]){
-         obj[sorted[i]] = rank;
+        if(!map.get(sorted[i])){
+         map.set(sorted[i], rank)
          rank++
         }
     }
-
     const res = []
     for(let i =0; i< arr.length; i++){
-       res.push(obj[arr[i]])
+       res.push(map.get(arr[i]))
     }
 
     return res
