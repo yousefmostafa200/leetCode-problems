0b3a3b8ae05ac6308de1f6896141639f079ff370
Yousef Mostafa <52266199+yousefmostafa200@users.noreply.github.com>
2024-09-06 23:07:48 +0300

    Time: 53 ms (44.47%), Space: 48.9 MB (38.59%) - LeetHub

---
+++ 1448-maximum-69-number/1448-maximum-69-number.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var maximum69Number  = function(num) {
+     
+  
+
+//   let result = num.toString().replace('6', '9');
+  
+
+  return parseInt(num.toString().replace('6', '9'));
+};
