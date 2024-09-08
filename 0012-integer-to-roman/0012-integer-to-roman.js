94fe983d7bc3cd72964fb5a452f173d2a0fac77a
Yousef Mostafa <52266199+yousefmostafa200@users.noreply.github.com>
2024-09-09 01:51:03 +0300

    Time: 108 ms (75.44%), Space: 55.2 MB (55.95%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function (num) {
+    const symbols = [['M', 1000], ['CM', 900], ['D', 500], ['CD', 400], ['C', 100], ['XC', 90],
+    ['L', 50], ['XL', 40], ['X', 10], ['IX', 9], ['V', 5], ['IV', 4], ['I', 1]];
+    let result = '';
+
+    for (let [symbol, value] of symbols) {
+        while (num >= value) {
+            result += symbol;
+            num -= value;
+        }
+    }
+
+    return result;
+};
