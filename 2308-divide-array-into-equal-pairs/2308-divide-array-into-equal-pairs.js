05777ddf685613e437d6770dae7651bbea933e01
Yousef Mostafa <52266199+yousefmostafa200@users.noreply.github.com>
2024-09-09 22:08:51 +0300

    Time: 51 ms (92.37%), Space: 52.3 MB (28.51%) - LeetHub

--- 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.js
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.js
@@ -5,13 +5,8 @@
 var divideArray = function(nums) {
     const freq = new Map();
     for(let num of nums){
-        freq.set(num, (freq.get(num) || 0) + 1)
+        // freq.set(num, (freq.get(num) || 0) + 1)
+        freq.has(num) ? freq.delete(num) : freq.set(num, true)
     }
-
-    for(let count of freq.values()){
-        if(count % 2 !== 0) return false
-    }
-
-
-   return true 
+  return freq.size === 0;
 };
