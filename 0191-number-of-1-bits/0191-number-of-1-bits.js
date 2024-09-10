fe885f09012e22c552b1404dda501c09b2f36577
Yousef Mostafa <52266199+yousefmostafa200@users.noreply.github.com>
2024-09-10 23:52:22 +0300

    Time: 54 ms (71.67%), Space: 49.3 MB (55.21%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var hammingWeight = function(n) {
+      let count = 0;
+    
+    
+    while (n !== 0) {
+        count += n & 1;  
+        n = n >>> 1;    
+    }
+    
+    return count;
+};
