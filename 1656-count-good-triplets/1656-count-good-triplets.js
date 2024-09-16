b916837ef5b3340914f2b0735c3fc896c25bab90
Yousef Mostafa <52266199+yousefmostafa200@users.noreply.github.com>
2024-09-16 21:54:18 +0300

    Time: 54 ms (93.51%), Space: 49 MB (61.62%) - LeetHub

--- 1656-count-good-triplets/1656-count-good-triplets.js
+++ 1656-count-good-triplets/1656-count-good-triplets.js
@@ -5,16 +5,15 @@
  * @param {number} c
  * @return {number}
  */
-var countGoodTriplets = function(arr, a, b, c) {
-      let count = 0;
+var countGoodTriplets = function (arr, a, b, c) {
+    let count = 0;
     for (let i = 0; i < arr.length; i++) {
         for (let j = i + 1; j < arr.length; j++) {
+            if (Math.abs(arr[i] - arr[j]) > a) continue
+
             for (let k = j + 1; k < arr.length; k++) {
-                if (Math.abs(arr[i] - arr[j]) <= a
-                    && Math.abs(arr[j] - arr[k]) <= b
-                    && Math.abs(arr[i] - arr[k]) <= c
-                )
-                    count++;
+                if (Math.abs(arr[j] - arr[k]) > b) continue
+                if (Math.abs(arr[k] - arr[i]) <= c) count++
             }
         }
     }
