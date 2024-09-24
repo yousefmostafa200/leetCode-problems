f8b325bb66982710f9110b719dd77d98c0b24e74
Yousef Mostafa <52266199+yousefmostafa200@users.noreply.github.com>
2024-09-24 22:49:46 +0300

    Time: 61 ms (17.62%), Space: 49.1 MB (74.9%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.js
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.js
@@ -5,8 +5,11 @@
  */
 var mergeAlternately = function(word1, word2) {
     let output = ''
+    let n = word1.length;
+    if ( n < word2.length )
+        n = word2.length;
 
-   for(let i =0; i < (Math.max(word1.length, word2.length)) ; i++){
+   for(let i =0; i < n ; i++){
     if(i < word1.length) output += word1[i]
     if(i < word2.length) output += word2[i]
    }
